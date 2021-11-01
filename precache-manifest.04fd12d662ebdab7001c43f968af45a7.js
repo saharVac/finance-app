d2db001f62db88114e43d514db53d0b6064b57ca
saharv-av <saharv@avanan.com>
2021-11-01 00:03:04 -0400

    Updates

---
+++ precache-manifest.04fd12d662ebdab7001c43f968af45a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63200b89adac0fc640fdec52b47e838",
+    "url": "/finance-app/index.html"
+  },
+  {
+    "revision": "c37c1d354c1d831881dc",
+    "url": "/finance-app/static/css/main.b4dfe803.chunk.css"
+  },
+  {
+    "revision": "4c7cec87dcbb8edd5556",
+    "url": "/finance-app/static/js/2.3999959c.chunk.js"
+  },
+  {
+    "revision": "727196b53be8768c78b2ea60f81f0ea1",
+    "url": "/finance-app/static/js/2.3999959c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37c1d354c1d831881dc",
+    "url": "/finance-app/static/js/main.f297bf62.chunk.js"
+  },
+  {
+    "revision": "aec6d52c7df449fddf2b",
+    "url": "/finance-app/static/js/runtime-main.3857e15d.js"
+  }
+]);
